5cac568f9f8d95303ee3dee15c3b50ba0ff7d0af
Trí Anh <anhmht@omnicasa.com>
2021-05-14 22:03:28 +0700

    implment validate login, info page

--- src/page/info/index.ts
+++ src/page/info/index.ts
@@ -1,14 +1,64 @@
 import Vue from "vue";
 import Component from "vue-class-component";
-import Carousel from "vue-owl-carousel";
+import polute from '../../../static/img/info-page/polute1.jpg';
+import { Carousel, Slide } from 'vue-carousel';
 
 @Component({
     template: require("./template.html").default,
     components: {
         "base-container": () => import("../../components/base-container/BaseContainerComponent.vue"),
-        Carousel
+        Carousel,
+        Slide
     }
 })
 export default class InfoPageComponent extends Vue {
+    navigateTo: number = 0;
+    posts: any = [
+        {
+            imageUrl: polute,
+            title: 'Ô nhiễm không khí tại một số đô thị lớn có xu hướng gia tăng'
+        },
+        {
+            imageUrl: polute,
+            title: 'Ô nhiễm không khí tại một số đô thị lớn có xu hướng gia tăng'
+        },
+        {
+            imageUrl: polute,
+            title: 'Ô nhiễm không khí tại một số đô thị lớn có xu hướng gia tăng'
+        },
+        {
+            imageUrl: polute,
+            title: 'Ô nhiễm không khí tại một số đô thị lớn có xu hướng gia tăng'
+        },
+        {
+            imageUrl: polute,
+            title: 'Ô nhiễm không khí tại một số đô thị lớn có xu hướng gia tăng'
+        },
+        {
+            imageUrl: polute,
+            title: 'Ô nhiễm không khí tại một số đô thị lớn có xu hướng gia tăng'
+        },
+        {
+            imageUrl: polute,
+            title: 'Ô nhiễm không khí tại một số đô thị lớn có xu hướng gia tăng'
+        },
+        {
+            imageUrl: polute,
+            title: 'Ô nhiễm không khí tại một số đô thị lớn có xu hướng gia tăng'
+        },
+    ];
 
+    handlePrev() {
+        if(this.navigateTo) {
+            this.navigateTo -= 1
+        }
+    }
+    handleNext() {
+        const totalPage = Math.ceil(this.posts.length / 4);
+        console.log(totalPage);
+
+        if (this.navigateTo < totalPage) {
+            this.navigateTo += 1
+        }
+    }
 }
--- src/page/login/index.ts
+++ src/page/login/index.ts
@@ -8,5 +8,24 @@ import Component from "vue-class-component";
     }
 })
 export default class LoginPageComponent extends Vue {
-    
+    valid: boolean = true;
+    data = {
+        userName: null,
+        password: null
+    }
+    rules = {
+        nameRules: [v => !!v || 'Vui lòng nhập tài khoản'],
+        passRules: [v => !!v || 'Vui lòng nhập mật khẩu']
+    }
+
+    handleLogin() {
+        //@ts-ignore
+        this.valid = this.$refs.loginForm.validate();
+        if(this.valid) {
+            console.log('thành công');
+        } else {
+            console.log('fail');
+        }
+    }
+
 }
