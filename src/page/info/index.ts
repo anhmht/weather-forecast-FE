0b27faa7ee37d8598ec170b8356a7fa8f0e30042
KhanhTH1 <khanh2402@gmail.com>
2021-05-14 17:13:31 +0900

    Add footer, lower half info page

--- src/page/info/index.ts
+++ src/page/info/index.ts
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import Component from "vue-class-component";
+import Carousel from "vue-owl-carousel";
 
 @Component({
     template: require("./template.html").default,
     components: {
-        "base-container": () => import("../../components/base-container/BaseContainerComponent.vue")
+        "base-container": () => import("../../components/base-container/BaseContainerComponent.vue"),
+        Carousel
     }
 })
 export default class InfoPageComponent extends Vue {
