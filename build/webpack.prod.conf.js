d14d973482f72f91e63a81e233addb072547267c
Trí Anh <anhmht@omnicasa.com>
2021-05-16 18:02:58 +0700

    add config package json

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -102,27 +102,4 @@ const webpackConfig = merge(baseWebpackConfig, {
     ]
 });
 
-if (process.env.NODE_ENV === 'production') {
-  const CompressionWebpackPlugin = require('compression-webpack-plugin')
-
-  webpackConfig.plugins.push(
-    new CompressionWebpackPlugin({
-      asset: '[path].gz[query]',
-      algorithm: 'gzip',
-      test: new RegExp(
-        '\\.(' +
-        config.build.productionGzipExtensions.join('|') +
-        ')$'
-      ),
-      threshold: 10240,
-      minRatio: 0.8
-    })
-  )
-}
-
-if (config.build.bundleAnalyzerReport) {
-  const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-  webpackConfig.plugins.push(new BundleAnalyzerPlugin())
-}
-
 module.exports = webpackConfig
