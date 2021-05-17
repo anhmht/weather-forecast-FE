426dedfc5412344de34b57dd72f794f2b33295a6
KhanhTH1 <khanh2402@gmail.com>
2021-05-18 08:27:06 +0900

    Add social page

--- src/page/social/index.ts
+++ src/page/social/index.ts
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Component from "vue-class-component";
+import image from "../../../static/img/social-page/item.jpg"
 
 @Component({
     template: require("./template.html").default,
@@ -8,5 +9,54 @@ import Component from "vue-class-component";
     }
 })
 export default class SocialPageComponent extends Vue {
+    mainPosts: any = [
+        {
+            imageUrl: image,
+            title: "Chiều tối và đêm 13/5, các khu vực đều mưa dông, đề phòng thời tiết nguy hiểm",
+            date: "16.05.2021"
+        },
+        {
+            imageUrl: image,
+            title: "Chiều tối và đêm 13/5, các khu vực đều mưa dông, đề phòng thời tiết nguy hiểm",
+            date: "16.05.2021"
+        }
+    ];
 
+    subPostsFirstRow: any = [
+        {
+            imageUrl: image,
+            title: "Chiều tối và đêm 13/5, các khu vực đều mưa dông, đề phòng thời tiết nguy hiểm",
+        },
+        {
+            imageUrl: image,
+            title: "Chiều tối và đêm 13/5, các khu vực đều mưa dông, đề phòng thời tiết nguy hiểm",
+        },
+        {
+            imageUrl: image,
+            title: "Chiều tối và đêm 13/5, các khu vực đều mưa dông, đề phòng thời tiết nguy hiểm",
+        },
+        {
+            imageUrl: image,
+            title: "Chiều tối và đêm 13/5, các khu vực đều mưa dông, đề phòng thời tiết nguy hiểm",
+        }
+    ];
+
+    subPostsSecondRow: any = [
+        {
+            imageUrl: image,
+            title: "Chiều tối và đêm 13/5, các khu vực đều mưa dông, đề phòng thời tiết nguy hiểm",
+        },
+        {
+            imageUrl: image,
+            title: "Chiều tối và đêm 13/5, các khu vực đều mưa dông, đề phòng thời tiết nguy hiểm",
+        },
+        {
+            imageUrl: image,
+            title: "Chiều tối và đêm 13/5, các khu vực đều mưa dông, đề phòng thời tiết nguy hiểm",
+        },
+        {
+            imageUrl: image,
+            title: "Chiều tối và đêm 13/5, các khu vực đều mưa dông, đề phòng thời tiết nguy hiểm",
+        }
+    ];
 }
