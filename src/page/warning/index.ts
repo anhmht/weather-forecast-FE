5fa71ae9ffb3f5bba248a6d73c428c8bc9efaf03
KhanhTH1 <khanh2402@gmail.com>
2021-06-02 10:01:33 +0900

    Layout for all warning

--- src/page/warning/index.ts
+++ src/page/warning/index.ts
@@ -5,6 +5,7 @@ import { PostServices } from '../../service/post-service/post.service';
 import IPost from "../../model/post/post.model";
 import { Post } from '../../model/post/post.model';
 import moment from "moment";
+import image from "../../../static/img/social-page/item.jpg";
 
 @Component({
     template: require("./template.html").default,
@@ -16,6 +17,29 @@ export default class WarningPageComponent extends Vue {
     postService: PostServices = new PostServices();
     postModel: IPost = new Post({});
     warningPosts: any = [];
+
+    mostWatchPosts: any = [
+        {
+            imageUrl: image,
+            title: "Áp thấp nhiệt đới mạnh lên thành bão, Trung bộ mưa lũ lớn"
+        },
+        {
+            imageUrl: image,
+            title: "Áp thấp nhiệt đới mạnh lên thành bão, Trung bộ mưa lũ lớn"
+        },
+        {
+            imageUrl: image,
+            title: "Áp thấp nhiệt đới mạnh lên thành bão, Trung bộ mưa lũ lớn"
+        },
+        {
+            imageUrl: image,
+            title: "Áp thấp nhiệt đới mạnh lên thành bão, Trung bộ mưa lũ lớn"
+        },
+        {
+            imageUrl: image,
+            title: "Áp thấp nhiệt đới mạnh lên thành bão, Trung bộ mưa lũ lớn"
+        },
+    ];
 
     handleViewDetail(postId) {
         this.$router.push({ name: ROUTE_NAME.INFO_DETAIL , params: { id: postId } })
