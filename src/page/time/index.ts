044d8d6b0583d774def08a89699f08a808a291d6
KhanhTH1 <khanh2402@gmail.com>
2021-06-03 23:01:24 +0900

    Loop to create array object in time component

--- src/page/time/index.ts
+++ src/page/time/index.ts
@@ -46,160 +46,26 @@ export default class TimePageComponent extends Vue {
 
         await this.forecastService.getTemperatureByStation(this.currentForecastStationId).then((res: any) => {
             const minMaxTempCurrentDate = DataHelper.getMinMaxTemp(res, DATE.CURRENT);
-            const minMaxTempNextDate = DataHelper.getMinMaxTemp(res, DATE.NEXT_DAY);
-            const minMaxTempNext2Date = DataHelper.getMinMaxTemp(res, DATE.NEXT_2_DAY);
-            const minMaxTempNext3Date = DataHelper.getMinMaxTemp(res, DATE.NEXT_3_DAY);
-            const minMaxTempNext4Date = DataHelper.getMinMaxTemp(res, DATE.NEXT_4_DAY);
 
             this.currentTemp = DataHelper.getTempByHour(res, 0);
             this.currentDayMinTemp = minMaxTempCurrentDate.min;
             this.currentDayMaxTemp = minMaxTempCurrentDate.max;
 
-            this.weatherByDay = [
-                {
-                    day: moment().add(DATE.NEXT_DAY, 'days').format('dddd'),
+            for (let i = DATE.NEXT_DAY; i <= DATE.NEXT_4_DAY; i++) {
+                this.weatherByDay.push({
+                    day: moment().add(i, 'days').format('dddd'),
                     imageUrl: icon,
-                    temp: minMaxTempNextDate.min + '°C - ' + minMaxTempNextDate.max + '°C'
-                },
-                {
-                    day: moment().add(DATE.NEXT_2_DAY, 'days').format('dddd'),
-                    imageUrl: icon,
-                    temp: minMaxTempNext2Date.min + '°C - ' + minMaxTempNext2Date.max + '°C'
-                },
-                {
-                    day: moment().add(DATE.NEXT_3_DAY, 'days').format('dddd'),
-                    imageUrl: icon,
-                    temp: minMaxTempNext3Date.min + '°C - ' + minMaxTempNext3Date.max + '°C'
-                },
-                {
-                    day: moment().add(DATE.NEXT_4_DAY, 'days').format('dddd'),
-                    imageUrl: icon,
-                    temp: minMaxTempNext4Date.min + '°C - ' + minMaxTempNext4Date.max + '°C'
-                },
-            ];
+                    temp: DataHelper.getMinMaxTemp(res, i).min + '°C - ' + DataHelper.getMinMaxTemp(res, i).max + '°C'
+                });
+            }
 
-            this.weatherByTime = [
-                {
-                    time: DataHelper.getDisplayHour(0),
+            for (let i = 0; i < 24; i++) {
+                this.weatherByTime.push({
+                    time: DataHelper.getDisplayHour(i),
                     imageUrl: icon,
-                    temp: this.currentTemp
-                },
-                {
-                    time: DataHelper.getDisplayHour(1),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 1)
-                },
-                {
-                    time: DataHelper.getDisplayHour(2),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 2)
-                },
-                {
-                    time: DataHelper.getDisplayHour(3),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 3)
-                },
-                {
-                    time: DataHelper.getDisplayHour(4),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 4)
-                },
-                {
-                    time: DataHelper.getDisplayHour(5),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 5)
-                },
-                {
-                    time: DataHelper.getDisplayHour(6),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 6)
-                },
-                {
-                    time: DataHelper.getDisplayHour(7),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 7)
-                },
-                {
-                    time: DataHelper.getDisplayHour(8),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 8)
-                },
-                {
-                    time: DataHelper.getDisplayHour(9),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 9)
-                },
-                {
-                    time: DataHelper.getDisplayHour(10),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 10)
-                },
-                {
-                    time: DataHelper.getDisplayHour(11),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 11)
-                },
-                {
-                    time: DataHelper.getDisplayHour(12),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 12)
-                },
-                {
-                    time: DataHelper.getDisplayHour(13),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 13)
-                },
-                {
-                    time: DataHelper.getDisplayHour(14),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 14)
-                },
-                {
-                    time: DataHelper.getDisplayHour(15),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 15)
-                },
-                {
-                    time: DataHelper.getDisplayHour(16),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 16)
-                },
-                {
-                    time: DataHelper.getDisplayHour(17),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 17)
-                },
-                {
-                    time: DataHelper.getDisplayHour(18),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 18)
-                },
-                {
-                    time: DataHelper.getDisplayHour(19),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 19)
-                },
-                {
-                    time: DataHelper.getDisplayHour(20),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 20)
-                },
-                {
-                    time: DataHelper.getDisplayHour(21),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 21)
-                },
-                {
-                    time: DataHelper.getDisplayHour(22),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 22)
-                },
-                {
-                    time: DataHelper.getDisplayHour(23),
-                    imageUrl: icon,
-                    temp: DataHelper.getTempByHour(res, 23)
-                },
-            ];
+                    temp: DataHelper.getTempByHour(res, i)
+                });
+            }
         }).catch(error => {
             console.log(error);
         })
