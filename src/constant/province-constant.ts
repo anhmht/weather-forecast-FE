74dbc55d2879a85fbde5a97f54941dcac02059b0
KhanhTH1 <khanh2402@gmail.com>
2021-06-15 01:09:30 +0900

    Implement data component

--- src/constant/province-constant.ts
+++ src/constant/province-constant.ts
@@ -4,440 +4,503 @@ export const PROVINCE = [
         "slug": "vinh-phuc",
         "type": "tinh",
         "name_with_type": "Tỉnh Vĩnh Phúc",
-        "code": "26"
+        "code": "26",
+        "region": "DBSH"
     },
     {
         "name": "Ninh Bình",
         "slug": "ninh-binh",
         "type": "tinh",
         "name_with_type": "Tỉnh Ninh Bình",
-        "code": "37"
+        "code": "37",
+        "region": "DBSH"
     },
     {
         "name": "Cà Mau",
         "slug": "ca-mau",
         "type": "tinh",
         "name_with_type": "Tỉnh Cà Mau",
-        "code": "96"
+        "code": "96",
+        "region": "TNB"
     },
     {
         "name": "Nghệ An",
         "slug": "nghe-an",
         "type": "tinh",
         "name_with_type": "Tỉnh Nghệ An",
-        "code": "40"
+        "code": "40",
+        "region": "BTB"
     },
     {
         "name": "Vĩnh Long",
         "slug": "vinh-long",
         "type": "tinh",
         "name_with_type": "Tỉnh Vĩnh Long",
-        "code": "86"
+        "code": "86",
+        "region": "TNB"
     },
     {
         "name": "Quảng Bình",
         "slug": "quang-binh",
         "type": "tinh",
         "name_with_type": "Tỉnh Quảng Bình",
-        "code": "44"
+        "code": "44",
+        "region": "BTB"
     },
     {
         "name": "Hải Phòng",
         "slug": "hai-phong",
         "type": "thanh-pho",
         "name_with_type": "Thành phố Hải Phòng",
-        "code": "31"
+        "code": "31",
+        "region": "DBSH"
     },
     {
         "name": "Hà Nội",
         "slug": "ha-noi",
         "type": "thanh-pho",
         "name_with_type": "Thành phố Hà Nội",
-        "code": "01"
+        "code": "01",
+        "region": "DBSH"
     },
     {
         "name": "Đồng Tháp",
         "slug": "dong-thap",
         "type": "tinh",
         "name_with_type": "Tỉnh Đồng Tháp",
-        "code": "87"
+        "code": "87",
+        "region": "TNB"
     },
     {
         "name": "Lai Châu",
         "slug": "lai-chau",
         "type": "tinh",
         "name_with_type": "Tỉnh Lai Châu",
-        "code": "12"
+        "code": "12",
+        "region": "TBB"
     },
     {
         "name": "Hưng Yên",
         "slug": "hung-yen",
         "type": "tinh",
         "name_with_type": "Tỉnh Hưng Yên",
-        "code": "33"
+        "code": "33",
+        "region": "DBSH"
     },
     {
         "name": "Lạng Sơn",
         "slug": "lang-son",
         "type": "tinh",
         "name_with_type": "Tỉnh Lạng Sơn",
-        "code": "20"
+        "code": "20",
+        "region": "DBB"
     },
     {
         "name": "Đồng Nai",
         "slug": "dong-nai",
         "type": "tinh",
         "name_with_type": "Tỉnh Đồng Nai",
-        "code": "75"
+        "code": "75",
+        "region": "DNB"
     },
     {
         "name": "Tiền Giang",
         "slug": "tien-giang",
         "type": "tinh",
         "name_with_type": "Tỉnh Tiền Giang",
-        "code": "82"
+        "code": "82",
+        "region": "TNB"
     },
     {
         "name": "Phú Yên",
         "slug": "phu-yen",
         "type": "tinh",
         "name_with_type": "Tỉnh Phú Yên",
-        "code": "54"
+        "code": "54",
+        "region": "NTB"
     },
     {
         "name": "Yên Bái",
         "slug": "yen-bai",
         "type": "tinh",
         "name_with_type": "Tỉnh Yên Bái",
-        "code": "15"
+        "code": "15",
+        "region": "TBB"
     },
     {
         "name": "Bình Dương",
         "slug": "binh-duong",
         "type": "tinh",
         "name_with_type": "Tỉnh Bình Dương",
-        "code": "74"
+        "code": "74",
+        "region": "DNB"
     },
     {
         "name": "Quảng Nam",
         "slug": "quang-nam",
         "type": "tinh",
         "name_with_type": "Tỉnh Quảng Nam",
-        "code": "49"
+        "code": "49",
+        "region": "NTB"
     },
     {
         "name": "Điện Biên",
         "slug": "dien-bien",
         "type": "tinh",
         "name_with_type": "Tỉnh Điện Biên",
-        "code": "11"
+        "code": "11",
+        "region": "TBB"
     },
     {
         "name": "Kon Tum",
         "slug": "kon-tum",
         "type": "tinh",
         "name_with_type": "Tỉnh Kon Tum",
-        "code": "62"
+        "code": "62",
+        "region": "TN"
     },
     {
         "name": "Bạc Liêu",
         "slug": "bac-lieu",
         "type": "tinh",
         "name_with_type": "Tỉnh Bạc Liêu",
-        "code": "95"
+        "code": "95",
+        "region": "TNB"
     },
     {
         "name": "Đắk Lắk",
         "slug": "dak-lak",
         "type": "tinh",
         "name_with_type": "Tỉnh Đắk Lắk",
-        "code": "66"
+        "code": "66",
+        "region": "TN"
     },
     {
         "name": "An Giang",
         "slug": "an-giang",
         "type": "tinh",
         "name_with_type": "Tỉnh An Giang",
-        "code": "89"
+        "code": "89",
+        "region": "TNB"
     },
     {
         "name": "Thanh Hóa",
         "slug": "thanh-hoa",
         "type": "tinh",
         "name_with_type": "Tỉnh Thanh Hóa",
-        "code": "38"
+        "code": "38",
+        "region": "BTB"
     },
     {
         "name": "Quảng Ngãi",
         "slug": "quang-ngai",
         "type": "tinh",
         "name_with_type": "Tỉnh Quảng Ngãi",
-        "code": "51"
+        "code": "51",
+        "region": "NTB"
     },
     {
         "name": "Tây Ninh",
         "slug": "tay-ninh",
         "type": "tinh",
         "name_with_type": "Tỉnh Tây Ninh",
-        "code": "72"
+        "code": "72",
+        "region": "DNB"
     },
     {
         "name": "Bắc Giang",
         "slug": "bac-giang",
         "type": "tinh",
         "name_with_type": "Tỉnh Bắc Giang",
-        "code": "24"
+        "code": "24",
+        "region": "DBB"
     },
     {
         "name": "Hồ Chí Minh",
         "slug": "ho-chi-minh",
         "type": "thanh-pho",
         "name_with_type": "Thành phố Hồ Chí Minh",
-        "code": "79"
+        "code": "79",
+        "region": "DNB"
     },
     {
         "name": "Ninh Thuận",
         "slug": "ninh-thuan",
         "type": "tinh",
         "name_with_type": "Tỉnh Ninh Thuận",
-        "code": "58"
+        "code": "58",
+        "region": "NTB"
     },
     {
         "name": "Tuyên Quang",
         "slug": "tuyen-quang",
         "type": "tinh",
         "name_with_type": "Tỉnh Tuyên Quang",
-        "code": "08"
+        "code": "08",
+        "region": "DBB"
     },
     {
         "name": "Sơn La",
         "slug": "son-la",
         "type": "tinh",
         "name_with_type": "Tỉnh Sơn La",
-        "code": "14"
+        "code": "14",
+        "region": "TBB"
     },
     {
         "name": "Nam Định",
         "slug": "nam-dinh",
         "type": "tinh",
         "name_with_type": "Tỉnh Nam Định",
-        "code": "36"
+        "code": "36",
+        "region": "DBSH"
     },
     {
         "name": "Hà Nam",
         "slug": "ha-nam",
         "type": "tinh",
         "name_with_type": "Tỉnh Hà Nam",
-        "code": "35"
+        "code": "35",
+        "region": "DBSH"
     },
     {
         "name": "Hải Dương",
         "slug": "hai-duong",
         "type": "tinh",
         "name_with_type": "Tỉnh Hải Dương",
-        "code": "30"
+        "code": "30",
+        "region": "DBSH"
     },
     {
         "name": "Bà Rịa - Vũng Tàu",
         "slug": "ba-ria-vung-tau",
         "type": "tinh",
         "name_with_type": "Tỉnh Bà Rịa - Vũng Tàu",
-        "code": "77"
+        "code": "77",
+        "region": "DNB"
     },
     {
         "name": "Kiên Giang",
         "slug": "kien-giang",
         "type": "tinh",
         "name_with_type": "Tỉnh Kiên Giang",
-        "code": "91"
+        "code": "91",
+        "region": "TNB"
     },
     {
         "name": "Cao Bằng",
         "slug": "cao-bang",
         "type": "tinh",
         "name_with_type": "Tỉnh Cao Bằng",
-        "code": "04"
+        "code": "04",
+        "region": "DBB"
     },
     {
         "name": "Hà Tĩnh",
         "slug": "ha-tinh",
         "type": "tinh",
         "name_with_type": "Tỉnh Hà Tĩnh",
-        "code": "42"
+        "code": "42",
+        "region": "BTB"
     },
     {
         "name": "Gia Lai",
         "slug": "gia-lai",
         "type": "tinh",
         "name_with_type": "Tỉnh Gia Lai",
-        "code": "64"
+        "code": "64",
+        "region": "TN"
     },
     {
         "name": "Hoà Bình",
         "slug": "hoa-binh",
         "type": "tinh",
         "name_with_type": "Tỉnh Hoà Bình",
-        "code": "17"
+        "code": "17",
+        "region": "TBB"
     },
     {
         "name": "Sóc Trăng",
         "slug": "soc-trang",
         "type": "tinh",
         "name_with_type": "Tỉnh Sóc Trăng",
-        "code": "94"
+        "code": "94",
+        "region": "TNB"
     },
     {
         "name": "Bình Định",
         "slug": "binh-dinh",
         "type": "tinh",
         "name_with_type": "Tỉnh Bình Định",
-        "code": "52"
+        "code": "52",
+        "region": "NTB"
     },
     {
         "name": "Thái Bình",
         "slug": "thai-binh",
         "type": "tinh",
         "name_with_type": "Tỉnh Thái Bình",
-        "code": "34"
+        "code": "34",
+        "region": "DBSH"
     },
     {
         "name": "Phú Thọ",
         "slug": "phu-tho",
         "type": "tinh",
         "name_with_type": "Tỉnh Phú Thọ",
-        "code": "25"
+        "code": "25",
+        "region": "DBB"
     },
     {
         "name": "Hậu Giang",
         "slug": "hau-giang",
         "type": "tinh",
         "name_with_type": "Tỉnh Hậu Giang",
-        "code": "93"
+        "code": "93",
+        "region": "TNB"
     },
     {
         "name": "Cần Thơ",
         "slug": "can-tho",
         "type": "thanh-pho",
         "name_with_type": "Thành phố Cần Thơ",
-        "code": "92"
+        "code": "92",
+        "region": "TNB"
     },
     {
         "name": "Quảng Ninh",
         "slug": "quang-ninh",
         "type": "tinh",
         "name_with_type": "Tỉnh Quảng Ninh",
-        "code": "22"
+        "code": "22",
+        "region": "DBB"
     },
     {
         "name": "Quảng Trị",
         "slug": "quang-tri",
         "type": "tinh",
         "name_with_type": "Tỉnh Quảng Trị",
-        "code": "45"
+        "code": "45",
+        "region": "BTB"
     },
     {
         "name": "Lào Cai",
         "slug": "lao-cai",
         "type": "tinh",
         "name_with_type": "Tỉnh Lào Cai",
-        "code": "10"
+        "code": "10",
+        "region": "TBB"
     },
     {
         "name": "Đắk Nông",
         "slug": "dak-nong",
         "type": "tinh",
         "name_with_type": "Tỉnh Đắk Nông",
-        "code": "67"
+        "code": "67",
+        "region": "TN"
     },
     {
         "name": "Bắc Ninh",
         "slug": "bac-ninh",
         "type": "tinh",
         "name_with_type": "Tỉnh Bắc Ninh",
-        "code": "27"
+        "code": "27",
+        "region": "DBSH"
     },
     {
         "name": "Lâm Đồng",
         "slug": "lam-dong",
         "type": "tinh",
         "name_with_type": "Tỉnh Lâm Đồng",
-        "code": "68"
+        "code": "68",
+        "region": "TN"
     },
     {
         "name": "Thái Nguyên",
         "slug": "thai-nguyen",
         "type": "tinh",
         "name_with_type": "Tỉnh Thái Nguyên",
-        "code": "19"
+        "code": "19",
+        "region": "DBB"
     },
     {
         "name": "Bình Thuận",
         "slug": "binh-thuan",
         "type": "tinh",
         "name_with_type": "Tỉnh Bình Thuận",
-        "code": "60"
+        "code": "60",
+        "region": "NTB"
     },
     {
         "name": "Khánh Hòa",
         "slug": "khanh-hoa",
         "type": "tinh",
         "name_with_type": "Tỉnh Khánh Hòa",
-        "code": "56"
+        "code": "56",
+        "region": "NTB"
     },
     {
         "name": "Bến Tre",
         "slug": "ben-tre",
         "type": "tinh",
         "name_with_type": "Tỉnh Bến Tre",
-        "code": "83"
+        "code": "83",
+        "region": "TNB"
     },
     {
         "name": "Thừa Thiên Huế",
         "slug": "thua-thien-hue",
         "type": "tinh",
         "name_with_type": "Tỉnh Thừa Thiên Huế",
-        "code": "46"
+        "code": "46",
+        "region": "BTB"
     },
     {
         "name": "Hà Giang",
         "slug": "ha-giang",
         "type": "tinh",
         "name_with_type": "Tỉnh Hà Giang",
-        "code": "02"
+        "code": "02",
+        "region": "DBB"
     },
     {
         "name": "Trà Vinh",
         "slug": "tra-vinh",
         "type": "tinh",
         "name_with_type": "Tỉnh Trà Vinh",
-        "code": "84"
+        "code": "84",
+        "region": "TNB"
     },
     {
         "name": "Long An",
         "slug": "long-an",
         "type": "tinh",
         "name_with_type": "Tỉnh Long An",
-        "code": "80"
+        "code": "80",
+        "region": "TNB"
     },
     {
         "name": "Bắc Kạn",
         "slug": "bac-kan",
         "type": "tinh",
         "name_with_type": "Tỉnh Bắc Kạn",
-        "code": "06"
+        "code": "06",
+        "region": "DBB"
     },
     {
         "name": "Đà Nẵng",
         "slug": "da-nang",
         "type": "thanh-pho",
         "name_with_type": "Thành phố Đà Nẵng",
-        "code": "48"
+        "code": "48",
+        "region": "NTB"
     },
     {
         "name": "Bình Phước",
         "slug": "binh-phuoc",
         "type": "tinh",
         "name_with_type": "Tỉnh Bình Phước",
-        "code": "70"
+        "code": "70",
+        "region": "DNB"
     }
 ]
--- src/page/data/index.ts
+++ src/page/data/index.ts
@@ -1,9 +1,10 @@
 import { namespace, Action, Getter } from 'vuex-class';
-import { PROVINCE } from './../../constant/province-constant';
 import Vue from "vue";
 import Component from "vue-class-component";
 import { storeModules } from '@/store';
 import lookupTypesStore from '@/store/lookup/lookup-types.store';
+import { PROVINCE } from '@/constant/province-constant';
+import { MonitoringServices } from '@/service/monitoring-service/monitoring.service';
 
 const LookupAction = namespace(storeModules.Lookup, Action);
 const LookupGetter = namespace(storeModules.Lookup, Getter);
@@ -14,13 +15,60 @@ const LookupGetter = namespace(storeModules.Lookup, Getter);
     }
 })
 export default class DataPageComponent extends Vue {
-    province = PROVINCE;
-    activeTab: number = 0
-
+    region: string = null;
+    allProvinces = PROVINCE;
+    province: number = null;
     @LookupAction getLookupData: (type: string) => Promise<void>
     @LookupGetter(lookupTypesStore.Get.KTTV) stations
+    monitoringService: MonitoringServices = new MonitoringServices();
 
-    mounted() {
+    allRegions: any = [
+        {
+            name: "Đông Bắc Bộ",
+            value: "DBB"
+        },
+        {
+            name: "Tây Bắc Bộ",
+            value: "TBB"
+        },
+        {
+            name: "Đồng bằng sông Hồng",
+            value: "DBSH"
+        },
+        {
+            name: "Bắc Trung Bộ",
+            value: "BTB"
+        },
+        {
+            name: "Nam Trung Bộ",
+            value: "NTB"
+        },
+        {
+            name: "Tây Nguyên",
+            value: "TN"
+        },
+        {
+            name: "Đông Nam Bộ",
+            value: "DNB"
+        },
+        {
+            name: "Tây Nam Bộ",
+            value: "TNB"
+        },
+    ]
+
+    get ProvincesByRegion() {
+        return this.allProvinces.filter(p => p.region === this.region);
+    }
+
+    get StationsByProvince() {
+        if (!isNaN(Number(this.province)) && this.stations) {
+            return this.stations.filter(s => s.zipCode === Number(this.province));
+        }
+        return [];
+    }
+
+    async mounted() {
         this.getLookupData(lookupTypesStore.Set.KTTV);
     }
 }
