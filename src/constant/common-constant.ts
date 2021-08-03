a71fb4ea79adf6f3277f9e22af3d270d92bfe213
anhmht <anhmht@omnicasa.com>
2021-08-03 22:07:46 +0700

    init dev version

--- src/constant/common-constant.ts
+++ src/constant/common-constant.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://weathermanagement.azurewebsites.net/';
+export const BASE_URL = 'https://weathermanagementdev.azurewebsites.net/';
 export const BASE_CLOUD_URL = 'https://weatherstoragevn.blob.core.windows.net/';
 export const DATE = {
     CURRENT: 0,
