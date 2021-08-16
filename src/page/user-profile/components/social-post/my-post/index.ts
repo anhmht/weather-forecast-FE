8e176122ab2029150a79255350326e6980608a8f
KhanhTH1 <khanh2402@gmail.com>
2021-08-16 21:23:01 +0700

    Implement social post in user profile

--- src/page/user-profile/components/social-post/my-post/index.ts
+++ src/page/user-profile/components/social-post/my-post/index.ts
@@ -1,21 +1,56 @@
 import Component from "vue-class-component";
-import Vue from 'vue';
+import Vue from "vue";
+import { SocialServices } from "@/service/social-service/social.service";
 
 @Component({
-    template: require('./template.html').default
+    template: require("./template.html").default
 })
 export default class MyPostComponent extends Vue {
+    GET_LIST_POST_ADMIN_LIMIT: number = 10;
+    GET_LIST_POST_ADMIN_PAGE: number = 1;
+    GET_LIST_POST_ADMIN_COMMENT_LIMIT: number = 10;
+    isReadMore: boolean = false;
+    myPosts: any = [];
+    socialService: SocialServices = new SocialServices();
 
     getStatus(statusId) {
         switch (statusId) {
             case 0:
-                return 'Chờ duyệt';
+                return "Chờ duyệt";
             case 1:
-                return 'Đã duyệt';
+                return "Đã duyệt";
             case 2:
-                return 'Không duyệt';
+                return "Không duyệt";
             default:
-                return '';
+                return "";
         }
+    }
+
+    displayContent(content) {
+        if (content.length >= 255) {
+            return `${content.substring(0, 254)}...`;
+        }
+
+        return content;
+    }
+
+    toggleContent() {
+        this.isReadMore = !this.isReadMore;
+    }
+
+    mounted() {
+        this.socialService
+            .getListPostsUser(
+                this.GET_LIST_POST_ADMIN_LIMIT,
+                this.GET_LIST_POST_ADMIN_PAGE,
+                this.GET_LIST_POST_ADMIN_COMMENT_LIMIT
+            )
+            .then((res: any) => {
+                this.myPosts = res.posts;
+                console.log(this.myPosts);
+            })
+            .catch(error => {
+                this.$errorMessage(error);
+            });
     }
 }
