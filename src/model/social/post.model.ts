63c6451be4c2da92cb7e99243fb733f1dac5d29d
DESKTOP-0IH2RMP\dzinhtoan <dzinhtoan@gmail.com>
2021-08-18 00:12:38 +0700

    [FE] - Social - Create post

--- src/model/social/post.model.ts
+++ src/model/social/post.model.ts
@@ -1,19 +1,19 @@
-export default interface IPost {
-    postId: string;
-    content: string;
-    imageUrls: string[];
-    videoUrls: string[];
+export interface ISocialPost {
+    id?: string;
+    content?: string;
+    imageUrls?: string[];
+    videoUrls?: string[];
 }
 
-export class Post implements IPost {
-    postId: string;
+export class SocialPost implements ISocialPost {
+    id?: string;
     content: string;
     imageUrls: string[];
     videoUrls: string[];
 
     constructor(option?: any) {
-        this.postId = option.postId || null
-        this.content = option.content || null
+        this.id = option.id || null
+        this.content = option.content || ''
         this.imageUrls = option.imageUrls || []
         this.videoUrls = option.videoUrls || []
     }
