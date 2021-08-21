16879b0a907574d054fac112ac68f98067b190c8
KhanhTH1 <khanh2402@gmail.com>
2021-08-21 14:50:13 +0700

    Implement social post in user profile

--- src/page/user-profile/components/social-post/my-post/index.ts
+++ src/page/user-profile/components/social-post/my-post/index.ts
@@ -1,21 +1,33 @@
 import Component from "vue-class-component";
 import Vue from "vue";
 import { SocialServices } from "@/service/social-service/social.service";
+import { storeModules } from "@/store";
+import { GeneralLookupTypes } from "@/store/lookup/lookup-types.store";
+import { Action, namespace } from "vuex-class";
+
+const LookupAction = namespace(storeModules.Lookup, Action);
 
 @Component({
     template: require("./template.html").default,
     components: {
-        "comment": () => import("../../../../../components/comment/CommentComponent.vue")
+        "comment": () => import("../../../../../components/comment/CommentComponent.vue"),
+        "reaction": () => import("../../../../../components/reaction/ReactionComponent.vue"),
+        "preview-image": () => import("../../../../../components/preview-image/PreviewImage.vue"),
+        "media-layout": () => import("../../../../../components/media-layout/MediaLayoutComponent.vue")
     }
 })
 export default class MyPostComponent extends Vue {
     GET_LIST_POST_ADMIN_LIMIT: number = 10;
     GET_LIST_POST_ADMIN_PAGE: number = 1;
     GET_LIST_POST_ADMIN_COMMENT_LIMIT: number = 10;
-    isReadMore: boolean = false;
-    isCommentable: boolean = false;
     myPosts: any = [];
+    isPreview: boolean = false;
+    isDisplayComment: boolean = false;
+    selectedItem: any = [];
+    selectedIndex: number = 0;
     socialService: SocialServices = new SocialServices();
+
+    @LookupAction getGeneralLookup: (payload: number[]) => Promise<void>;
 
     getStatus(statusId) {
         switch (statusId) {
@@ -38,16 +50,32 @@ export default class MyPostComponent extends Vue {
         return content;
     }
 
-    toggleContent(event) {
-        this.isReadMore = !this.isReadMore;
+    toggleContent(target, item) {
+        if (target.innerText === "Đọc thêm") {
+            target.parentElement.firstElementChild.innerText = item.content;
+            target.innerText = "Rút gọn";
+        } else if (target.innerText === "Rút gọn") {
+            target.parentElement.firstElementChild.innerText = this.displayContent(item.content);
+            target.innerText = "Đọc thêm";
+        }
     }
 
-    toggleComment() {
-        console.log("Comment clicked");
-        this.isCommentable = !this.isCommentable;
+    toggleComment(target) {
+        if (!this.isDisplayComment) {
+            target.parentElement.parentElement.nextElementSibling.style.display = 'none';
+        } else {
+            target.parentElement.parentElement.nextElementSibling.style.display = 'block';
+        }
+        this.isDisplayComment = !this.isDisplayComment;
     }
 
-    mounted() {
+    handlePreview(data) {
+        this.selectedItem = data.medias;
+        this.selectedIndex = data.index;
+        this.isPreview = true;
+    }
+
+    async mounted() {
         this.socialService
             .getListPostsUser(
                 this.GET_LIST_POST_ADMIN_LIMIT,
@@ -61,5 +89,10 @@ export default class MyPostComponent extends Vue {
             .catch(error => {
                 this.$errorMessage(error);
             });
+
+        const payload = [
+            GeneralLookupTypes.REACTION,
+        ];
+        await this.getGeneralLookup(payload);
     }
 }
