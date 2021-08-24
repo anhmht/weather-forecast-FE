f7401dca08e8ff7ab39bfcf93c7091b336371b0e
DESKTOP-0IH2RMP\dzinhtoan <dzinhtoan@gmail.com>
2021-08-25 01:21:57 +0700

    [FE] - Social - comment - Emoji

--- src/components/comment/index.ts
+++ src/components/comment/index.ts
@@ -5,8 +5,12 @@ import Component from "vue-class-component";
 
 @Component({
     template: require("./template.html").default,
+    components: {
+        "emoji-picker": () => import("../../components/emoji-picker/EmojiPickerComponent.vue")
+    }
 })
 export default class CommentComponent extends Vue {
+    newComment: string = '';
 
     getColor(name) {
         return DataHelper.generateColorByString(name);
@@ -14,5 +18,13 @@ export default class CommentComponent extends Vue {
 
     renderText(comment) {
         return comment.length >= 255 ? comment.slice(0, 255) + '...' : comment
+    }
+
+    handleClickOnEmoji (val) {
+        const vm = this;
+        if (vm.$refs.commentTextarea) {
+            let cnt = DataHelper.insertCharacterAtCursorPositionOfTextArea(vm.$refs.commentTextarea, val);
+            this.newComment = cnt;
+        }
     }
 }
